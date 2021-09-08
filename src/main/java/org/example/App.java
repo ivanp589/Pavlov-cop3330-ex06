ff7e4de39982b5b0d9bb26bf82f6ebf2b63c3ac6
ivanp589 <ivanpav@Knights.ucf.edu>
2021-09-08 12:49:01 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.time.Year;
+import java.time.temporal.TemporalField;
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    static Scanner in = new Scanner(System.in);
+
+    public static void main( String[] args )
+    {
+        App ask = new App();
+        int a = ask.askage();
+        int b = ask.askRetire();
+        int year = Year.now().getValue();
+        String outp = ask.stringf(a,b,year);
+        out.println( outp);
+    }
+
+    private int askage(){
+        out.print("What is your current age? ");
+        int age = in.nextInt();
+        return age;
+    }
+    private int askRetire(){
+        out.print("At what age would you like to retire? ");
+        int retire = in.nextInt();
+        return retire;
+    }
+
+    private String stringf(int a, int b,int Year){
+        return String.format("You have %d years left until you can retire.\n" +
+                "It's %d, so you can retire in %d.", b-a, Year,Year+(b-a));
+    }
+}
